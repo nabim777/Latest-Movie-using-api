f95d3f11d47f4567b7545f57786f03569f668f2e
nabim777 <nabinalemagar019@gmail.com>
2022-07-04 00:23:10 +0545

    Doctor Strange

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:apimovie/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
